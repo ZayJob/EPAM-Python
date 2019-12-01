dc836a437a8527fbfc458e2c7470c641da2fd277
Vlad Bubeniuk <ZayByst@mail.ru>
2019-12-01 17:38:24 +0300

    Add index.html

--- flask_web_app/app/views.py
+++ flask_web_app/app/views.py
@@ -1,13 +1,31 @@
 from app import app
 from pymongo import MongoClient
-from flask import render_template
-
-#client = MongoClient("mongo", 27017)
-#db = client["lol"]
-#collection = db["kek"]
-#collection.insert_one({"lol": "kek"})
+from flask import render_template, redirect, request
+from typing import Dict, Any
 
 
 @app.route("/")
 def index():
+    client = MongoClient("mongo", 27017)
+    db = client["News_feeds"]
+    collection = db["News"]
+
+    req = request.args
+
+    collection.insert({"args": parse_args_from_request(req)})
+
     return render_template('index.html')
+
+
+def parse_args_from_request(req):
+    dict_args = {
+        "source": req.get('url'),
+        "limit": req.get('limit'),
+        "date": req.get('date'),
+        "to_pdf": req.get('to_pdf')
+    }
+    return dict_args
+
+
+def run():
+    pass
